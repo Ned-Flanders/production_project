929b223cf38f00e2ad41ffbdf64ed48570d7c6bd
jamie <Jamie.w97@outlook.com>
2020-04-27 15:23:07 +0100

    Created the intial score system, only implemented on the security page atm, also has a basic leaderboard, installed vue

--- app/Http/Controllers/SecurityController.php
+++ app/Http/Controllers/SecurityController.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Flags;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Redirect;
+use App\Events\ScoreUpdated;
+use Illuminate\Support\Facades\Auth;
 
 class SecurityController extends Controller
 {
@@ -10,8 +17,41 @@ class SecurityController extends Controller
         $this->middleware('auth');
     }
 
-    public function index()
+    public function index(Request $request)
     {
         return view('security');
+    }
+
+    public function store(Request $request)
+    {
+        $input = $request->input();
+
+        $rules = [
+            'flag' => 'required|exists:flags,flag',
+            'flagid' => 'exists:flags,challenge_name'
+        ];
+
+        $messages = [
+            'flag.exists' => "That isn't the right flag, make sure to include 'flag{'",
+            'flagid.exists' => "That flagid isn't correct, are you submitting another challenge's flag?"
+        ];
+
+        $validator = Validator::make($input, $rules, $messages);
+
+        if($validator->fails()) {
+            return Redirect::back()->withErrors($validator)->withInput();
+        }
+
+        $user = Auth::user();
+        $user->score = $user->score + 1;
+        $user->save();
+
+        event(new ScoreUpdated($user)); // broadcast `ScoreUpdated` event
+
+        return redirect()->back()->withSuccess('Success, Your score has been updated')->withValue($user->score);
+    }
+
+    public function leaderboard () {
+        return User::all(['id', 'name', 'score']);
     }
 }
